9ee6dfaef22d2c4d11d38a1be9394d8f3d6c9c2d
akib bhuiyan <akibbhuiyan3544@gmail.com>
2025-08-27 01:49:24 +0600

    add some new functionality

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("her js is connected");
+
+// a shared function getting any thing by id !!
+
+function getId(id){
+    const getid = document.getElementById(id);
+    return getid;
+}
+
+
+
+// 01- hart button functionality
+
+const hartBtns = document.getElementsByClassName("love-btn");
+
+for(let hartbtn of hartBtns){
+    hartbtn.addEventListener("click",
+        function(){
+            
+            let navHart = getId("nav-hart").innerText;
+            navHart = Number(navHart) + 1;
+            getId("nav-hart").innerText = navHart;
+
+
+        }
+    )
+}
+
+
+//02 - functionality
+
